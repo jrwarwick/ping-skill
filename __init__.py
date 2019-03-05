26dc7fd6255aef03b9f9eca3ea1d5acd0d4bda4b
jrwarwick <justin.warwick@gmail.com>
2019-03-05 20:55:18 +0000

    Increase PEP8 compliance.

--- __init__.py
+++ __init__.py
@@ -25,8 +25,7 @@ class PingSkill(MycroftSkill):
             .require("PingKeyword").require("key").build()
         self.register_intent(ping_intent, self.handle_ping_intent)
 
-    def handle_ping_intent(self, message):       
-#        import subprocess as sp
+    def handle_ping_intent(self, message):
         hosts = dict()
         f = open(join(dirname(__file__), "hosts.txt"), 'r')
         for line in f.readlines():
@@ -35,19 +34,19 @@ class PingSkill(MycroftSkill):
             l = line.split(",")
             hosts[l[0].strip()] = [l[1].strip(), l[2].strip()]
         f.close()
-        
+ 
         k = message.data.get("key").lower()
         LOGGER.info(k + ' from ' + str(message.data))
         LOGGER.info(pprint.PrettyPrinter().pprint(message))
         if k in hosts:
             if hosts[k][0] == '1':
                 response = requests.get(hosts[k][1])
-                data = {"response": response.reason.replace('OK','OKAY') + " " +
+                data = {"response": response.reason.replace('OK', 'OKAY') + " " +
                         str(response.status_code)}
                 self.speak_dialog("ServerResponse", data)
             else:
                 status,result = subprocess.getstatusoutput("ping -c1 -w2 "
-                                + hosts[k][1][(hosts[k][1]).find("//")+1:].replace('/',''))
+                                + hosts[k][1][(hosts[k][1]).find("//")+1:].replace('/', ''))
                 if status == 0:
                     data = {"response": result.split('/')[5]}
                     self.speak_dialog("PingResponse", data)
@@ -65,23 +64,23 @@ class PingSkill(MycroftSkill):
                             'host' in result_message):
                         self.speak(result_message)
         else:
-            # Internal aliasing/nicknames not matched, 
-            # Way too complex to parse spoken full URLs, 
-            # but maybe the user just uttered an "unregistered" but perfectly 
+            # Internal aliasing/nicknames not matched,
+            # Way too complex to parse spoken full URLs,
+            # but maybe the user just uttered an "unregistered" but perfectly
             # valid DNS name so let's try normal public hierarchy resolution.
             # Just exit if keyword not found, and we don't get an easy clean
             # successful name resolution. Kind of best-effort, no guarantees.
 
             # When we are doing "real" ping we don't use URLs, just hostnames.
-            # Mycroft normalization is pretty good already, however, 
-            # there will remain challenge that `slashdot.com` is difficult 
+            # Mycroft normalization is pretty good already, however,
+            # there will remain challenge that `slashdot.com` is difficult
             # to parse.
-            # Since normalization might break content a bit, 
+            # Since normalization might break content a bit,
             # such as when "the" is actually part of domain name
             # we shall fall back to rough self-processing of "raw" utterance.
             k = (
-                message.data.get('utterance').strip().replace("ping ","")
-                .replace("dot",".").replace(" ","")
+                message.data.get('utterance').strip().replace("ping ", "")
+                .replace("dot", ".").replace(" ", "")
             )
             # TODO: finally look for and replace close homophones for
             # for well known TLD strings$ occurring at end of utterance
@@ -117,3 +116,4 @@ class PingSkill(MycroftSkill):
 
 def create_skill():
     return PingSkill()
+
